e3594bb2290551c83329d3f3470bb31f0609ac25
betterdataco <75857176+betterdataco@users.noreply.github.com>
2024-05-01 12:55:23 -0700

    auth changes, 04.30.2024

--- packages/utils/src/constants/middleware.ts
+++ packages/utils/src/constants/middleware.ts
@@ -1,15 +1,15 @@
 export const DEFAULT_REDIRECTS = {
   home: "https://betterdata.co",
   betterdata: "https://betterdata.co",
-  signin: "https://betterdata.co/login",
-  login: "https://betterdata.co/login",
-  register: "https://betterdata.co/register",
-  signup: "https://betterdata.co/register",
-  app: "https://betterdata.co",
-  dashboard: "https://betterdata.co",
-  links: "https://betterdata.co/links",
-  settings: "https://betterdata.co/settings",
-  welcome: "https://betterdata.co/welcome",
+  signin: "https://app.betterdata.co/login",
+  login: "https://app.betterdata.co/login",
+  register: "https://app.betterdata.co/register",
+  signup: "https://app.betterdata.co/register",
+  app: "https://app.betterdata.co",
+  dashboard: "https://app.betterdata.co",
+  links: "https://app.betterdata.co/links",
+  settings: "https://app.betterdata.co/settings",
+  welcome: "https://app.betterdata.co/welcome",
   discord: "https://twitter.com/betterdatadotco", // placeholder for now
 };
 
