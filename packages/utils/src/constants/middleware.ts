fb56e6b059ffb81bcca1593064a8d8a883344499
betterdataco <75857176+betterdataco@users.noreply.github.com>
2024-04-30 17:19:44 -0700

    auth changes, 04.30.2024

--- packages/utils/src/constants/middleware.ts
+++ packages/utils/src/constants/middleware.ts
@@ -1,20 +1,20 @@
 export const DEFAULT_REDIRECTS = {
-  home: "https://dub.co",
-  dub: "https://dub.co",
-  signin: "https://dub.co/login",
-  login: "https://dub.co/login",
-  register: "https://dub.co/register",
-  signup: "https://dub.co/register",
-  app: "https://dub.co",
-  dashboard: "https://dub.co",
-  links: "https://dub.co/links",
-  settings: "https://dub.co/settings",
-  welcome: "https://dub.co/welcome",
-  discord: "https://twitter.com/dubdotco", // placeholder for now
+  home: "https://app.betterdata.co",
+  betterdata: "https://app.betterdata.co",
+  signin: "https://app.betterdata.co/login",
+  login: "https://app.betterdata.co/login",
+  register: "https://app.betterdata.co/register",
+  signup: "https://app.betterdata.co/register",
+  app: "https://app.betterdata.co",
+  dashboard: "https://app.betterdata.co",
+  links: "https://app.betterdata.co/links",
+  settings: "https://app.betterdata.co/settings",
+  welcome: "https://app.betterdata.co/welcome",
+  discord: "https://twitter.com/betterdatadotco", // placeholder for now
 };
 
 export const DUB_HEADERS = {
   headers: {
-    "x-powered-by": "dub.co - Link management for modern marketing teams",
+    "x-powered-by": "app.betterdata.co - Link management for modern marketing teams",
   },
 };
